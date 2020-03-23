9f10479aa4d2450beb5842118bc5d2d0f073af67
Abdul Aziz <otakcerdascom@gmail.com>
2020-03-23 10:55:16 +0700

    tambah database

--- database/mnode.sql
+++ database/mnode.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.7.7
+-- version 5.0.1
 -- https://www.phpmyadmin.net/
 --
--- Host: localhost
--- Waktu pembuatan: 28 Bulan Mei 2019 pada 06.39
--- Versi server: 10.1.30-MariaDB
--- Versi PHP: 7.2.2
+-- Host: 127.0.0.1
+-- Waktu pembuatan: 23 Mar 2020 pada 04.53
+-- Versi server: 10.4.11-MariaDB
+-- Versi PHP: 7.4.3
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -19,7 +19,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `tachi`
+-- Database: `mnode`
 --
 
 -- --------------------------------------------------------
